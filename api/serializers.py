87b97778e0bd5a4353585ca72eabafa991ba1989
claudejin <mgjin@se.kaist.ac.kr>
2017-05-24 22:45:06 +0900

    Full model of Machine, Failure, Userguide, Alternative

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from rest_framework import serializers
-from .models import Machine
+from .models import Machine, Failure, Userguide, Alternative
 
 class MachineSerializer(serializers.ModelSerializer):
     """Serializer to map the Model instance into JSON format."""
@@ -10,5 +10,20 @@ class MachineSerializer(serializers.ModelSerializer):
     class Meta:
         """Meta class to map serializer's fields with the model fields."""
         model = Machine
-        fields = ('id', 'name')
+        fields = ('id', 'serialNum', 'location', 'inUse', 'isBroken')
         read_only_fields = ()
+
+class FailureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Failure
+        fields = ('id', 'machine', 'type', 'comment', 'reporterId', 'isFixed')
+
+class UserguideSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Userguide
+        fields = ('id', 'guideName', 'content')
+
+class AlternativeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Alternative
+        fields = ('id', 'serviceName', 'location', 'phoneNum', 'price')
